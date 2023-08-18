c161e4e377bb044d493266077d4400775eb22b97
Prakhar <prakharb56@gmail.com>
2023-08-18 13:25:08 +0530

    added icons

---
+++ src/utils/data.ts
@@ -0,0 +1,74 @@
+import { RowType } from "@/types/types";
+
+export const data: RowType[] = [
+  {
+    studentData: {
+      program: {
+        value: "Himachal Students",
+        label: "Himachal Students",
+      },
+      batch: {
+        value: "DL-11-Alpha-eng-23",
+        label: "DL-11-Alpha-eng-23",
+      },
+      grade: {
+        value: "10",
+        label: "10",
+      },
+      course: {
+        value: "Alpha",
+        label: "Alpha",
+      },
+      stream: {
+        value: "Medical",
+        label: "Medical",
+      },
+    },
+    testData: {
+      testName: "Test_name 1515",
+      testLink: "Test Link",
+      testTaker: "test_abc",
+      testType: {
+        value: "homework",
+        label: "Homework",
+      },
+      testFormat: {
+        value: "major_test",
+        label: "Major Test",
+      },
+      testPurpose: {
+        value: "weekly_test",
+        label: "Weekly Test",
+      },
+      testPlatform: {
+        value: "Quiz",
+        label: "Quiz",
+      },
+      markingScheme: {
+        value: "neet",
+        label: "NEET",
+      },
+      optionalLimit: {
+        value: "4,-1",
+        label: "4 , -1",
+      },
+    },
+    timelineData: {
+      startDate: "test_date s",
+      endDate: "test_date e",
+      reportLink: "report link",
+      isEnabled: {
+        value: true,
+        label: "ON",
+      },
+      sessionType: {
+        value: "infinite",
+        label: "Infinite",
+      },
+      synced: {
+        value: false,
+        label: "FALSE",
+      },
+    },
+  },
+];
