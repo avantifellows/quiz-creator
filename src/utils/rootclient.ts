a659528f8c233e69790bf55a236f041e5ee6074e
Prakhar <prakharb56@gmail.com>
2023-09-06 00:29:35 +0530

    reviewed changes

--- src/utils/rootclient.ts
+++ src/utils/rootclient.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-// export const axios_ = axios.create({
-//   baseURL: "http://localhost:3001",
-// });
-
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
