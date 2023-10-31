38044432b5ad3920460e4bd6cfad65b601338c29
Prakhar <prakharb56@gmail.com>
2023-10-31 19:07:00 +0530

    build fixes v2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import DataDisplay from "@/components/displayTable/DataDisplay";
-import { getData } from "@/utils/FormInputHandling";
+import { getData } from "../utils/FormInputHandling";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
