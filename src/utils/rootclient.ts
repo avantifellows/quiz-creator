8dd464b61dec6c60ffba8fdf0dac0e87d7c7564e
Prakhar <prakharb56@gmail.com>
2023-11-02 14:39:17 +0530

    Remove fileinputhandling and rootclient

--- src/utils/rootclient.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_DB_URL,
-  headers: { Authorization: "Bearer " + process.env.NEXT_PUBLIC_BEARER_TOKEN },
-});
