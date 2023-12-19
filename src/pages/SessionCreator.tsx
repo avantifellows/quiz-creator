d4e6845b240dab170f2b60938adfb2c6c4c70d46
Prakhar <prakharb56@gmail.com>
2023-12-19 14:05:28 +0530

    removed log statements

--- src/pages/SessionCreator.tsx
+++ src/pages/SessionCreator.tsx
@@ -42,7 +42,6 @@ export default function SessionCreator() {
     const postResult = await response.json();
 
     const sessionId = postResult.id;
-    console.log(sessionId);
 
     setIsSessionAdded(true);
 
