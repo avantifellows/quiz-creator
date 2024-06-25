f4094248471b1adf750d791acd963ca6cca27afb
Drish-xD <drish.xd@gmail.com>
2024-06-26 00:33:37 +0530

    add: validations

--- src/types/form.types.ts
+++ src/types/form.types.ts
@@ -12,45 +12,75 @@ import {
   TestTypeOptions,
 } from '@/Constants';
 import { z } from 'zod';
+import { Platform } from './enums';
 
-export const basicSchema = z.object({
-  group: z.string({ required_error: 'This field is required' }),
-  parentBatch: z.string({ required_error: 'This field is required' }),
-  subBatch: z.string({ required_error: 'This field is required' }),
-  grade: z.coerce
-    .number({ required_error: 'This field is required' })
-    .refine(
-      (value) => GradeOptions.some((option) => option.value === value),
-      'Invalid option selected'
-    ),
-  sessionType: z
-    .string({ required_error: 'This field is required' })
-    .refine(
-      (value) => SessionTypeOptions.some((option) => option.value === value),
-      'Invalid option selected'
-    ),
-  authType: z.string({ required_error: 'This field is required' }),
-  activateSignUp: z.coerce.boolean(),
-  isPopupForm: z.coerce.boolean(),
-  noOfFieldsInPopup: z.coerce
-    .number({
-      required_error: 'This field is required',
-      message: 'This is not a valid number',
-    })
-    .int()
-    .min(0)
-    .or(z.string().nullable()),
-  isRedirection: z.coerce.boolean(),
-  isIdGeneration: z.coerce.boolean(),
-  signupFormId: z.coerce.number().optional().nullable(),
-  popupFormId: z.coerce.number().optional().nullable(),
-  platform: z
-    .string({ required_error: 'This field is required' })
-    .refine(
-      (value) => TestPlatformOptions.some((option) => option.value === value),
-      'Invalid option selected'
-    ),
-});
+export const basicSchema = z
+  .object({
+    group: z.string({ required_error: 'This field is required' }),
+    parentBatch: z.string({ required_error: 'This field is required' }),
+    subBatch: z.string({ required_error: 'This field is required' }),
+    grade: z.coerce
+      .number({ required_error: 'This field is required' })
+      .refine(
+        (value) => GradeOptions.some((option) => option.value === value),
+        'Invalid option selected'
+      ),
+    sessionType: z
+      .string({ required_error: 'This field is required' })
+      .refine(
+        (value) => SessionTypeOptions.some((option) => option.value === value),
+        'Invalid option selected'
+      ),
+    authType: z.string({ required_error: 'This field is required' }),
+    activateSignUp: z.coerce.boolean(),
+    isPopupForm: z.coerce.boolean(),
+    noOfFieldsInPopup: z.coerce
+      .number({
+        required_error: 'This field is required',
+        message: 'This is not a valid number',
+      })
+      .int()
+      .min(0)
+      .or(z.string().nullable()),
+    isRedirection: z.coerce.boolean(),
+    isIdGeneration: z.coerce.boolean(),
+    signupFormId: z.coerce.number().optional().nullable(),
+    popupFormId: z.coerce.number().optional().nullable(),
+    platform: z
+      .string({ required_error: 'This field is required' })
+      .refine(
+        (value) => TestPlatformOptions.some((option) => option.value === value),
+        'Invalid option selected'
+      ),
+  })
+  .superRefine((data, context) => {
+    if (data.isPopupForm) {
+      if (!data.noOfFieldsInPopup) {
+        context.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'This field is required',
+          path: ['noOfFieldsInPopup'],
+        });
+      }
+      if (!data.popupFormId) {
+        context.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'This field is required',
+          path: ['popupFormId'],
+        });
+      }
+    }
+
+    if (data.activateSignUp) {
+      if (!data.signupFormId) {
+        context.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'This field is required',
+          path: ['signupFormId'],
+        });
+      }
+    }
+  });
 
 export const quizSchema = z.object({
   course: z
@@ -100,41 +130,67 @@ export const quizSchema = z.object({
   showAnswers: z.coerce.boolean(),
 });
 
-export const timelineSchema = z.object({
-  startDate: z.coerce.date({
-    required_error: 'This field is required',
-    message: 'This is not a valid date and time',
-  }),
-  endDate: z.coerce.date({
-    required_error: 'This field is required',
-    message: 'This is not a valid date and time',
-  }),
-  isEnabled: z.coerce.boolean(),
-  activeDays: z.array(
-    z
-      .number({ required_error: 'This field is required' })
-      .refine(
-        (value) => ActiveDaysOptions.some((option) => option.value === value),
-        'Invalid option selected'
-      )
-  ),
-  testTakers: z.coerce
-    .number({
+export const timelineSchema = z
+  .object({
+    startDate: z.coerce.date({
       required_error: 'This field is required',
-      message: 'This is not a valid number',
-    })
-    .int()
-    .min(0, 'Test Takers must be greater than 0'),
-});
+      message: 'This is not a valid date and time',
+    }),
+    endDate: z.coerce.date({
+      required_error: 'This field is required',
+      message: 'This is not a valid date and time',
+    }),
+    isEnabled: z.coerce.boolean(),
+    activeDays: z.array(
+      z
+        .number({ required_error: 'This field is required' })
+        .refine(
+          (value) => ActiveDaysOptions.some((option) => option.value === value),
+          'Invalid option selected'
+        )
+    ),
+    testTakers: z.coerce
+      .number({
+        required_error: 'This field is required',
+        message: 'This is not a valid number',
+      })
+      .int()
+      .min(0, 'Test Takers must be greater than 0'),
+  })
+  .refine((data) => data.startDate < data.endDate, {
+    message: 'End date must be greater than start date.',
+    path: ['endDate'],
+  });
 
-export const liveSchema = z.object({
-  platformLink: z
-    .string({ required_error: 'This field is required' })
-    .url('This is not a valid url'),
-  platformId: z.string({ required_error: 'This field is required' }),
-  subject: z.string({ required_error: 'This field is required' }),
-  name: z.string({ required_error: 'This field is required' }),
-});
+export const liveSchema = z
+  .object({
+    platformLink: z
+      .string({ required_error: 'This field is required' })
+      .url('This is not a valid url'),
+    platformId: z.string({ required_error: 'This field is required' }),
+    subject: z.string({ required_error: 'This field is required' }),
+    name: z.string({ required_error: 'This field is required' }),
+    platform: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      if (Platform.Meet === data.platform) {
+        return data.platformLink.includes('meet.google.com');
+      } else if (Platform.Zoom === data.platform) {
+        return data.platformLink.includes('zoom.us');
+      } else if (Platform.Youtube === data.platform) {
+        return data.platformLink.includes('youtube.com');
+      } else if (Platform.Plio === data.platform || Platform.SPlio === data.platform) {
+        return data.platformLink.includes('plio.in');
+      } else {
+        return true;
+      }
+    },
+    (data) => ({
+      message: `Platform link is not valid for ${data.platform}`,
+      path: ['platformLink'],
+    })
+  );
 
 export type basicFields = z.infer<typeof basicSchema>;
 export type quizFields = z.infer<typeof quizSchema>;
