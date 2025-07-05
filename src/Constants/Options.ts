af58599add749451c1859d998ad8cfa09452506c
suryabulusu <teja.surya59@gmail.com>
2025-07-05 13:39:51 +0530

    adding two options

--- src/Constants/Options.ts
+++ src/Constants/Options.ts
@@ -73,6 +73,8 @@ export const CourseOptions: Option[] = [
   { value: 'Photon', label: 'Photon' },
   { value: 'JEE', label: 'JEE' },
   { value: 'CUET', label: 'CUET' },
+  { value: 'CA', label: 'CA' },
+  { value: 'CLAT', label: 'CLAT' },
 ];
 
 export const StreamOptions: Option[] = [
@@ -92,6 +94,8 @@ export const StreamOptions: Option[] = [
   { value: 'business_studies', label: 'Business Studies' },
   { value: 'economics', label: 'Economics' },
   { value: 'nda', label: 'NDA' },
+  { value: 'ca', label: 'CA' },
+  { value: 'clat', label: 'CLAT' },
 ];
 
 export const GradeOptions = Grades.map((g) => ({ value: Number(g), label: g.toString() }));
